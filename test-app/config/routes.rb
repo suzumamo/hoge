84ca38f0664464cc23d73bb58c517b44dca56dd7
suzumamo <m.suzuki@artwize.co.jp>
2018-06-19 05:53:06 +0000

    second commit

--- test-app/config/routes.rb
+++ test-app/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   #root 'application#hello'# ※ルーティングは二つ以上設定できないよう。
   #root 'application#goodbye'
   # MemosControllerのindexアクションをルートで実行する
+  # aa
   root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
